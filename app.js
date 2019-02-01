74107e9adddb9893905cf347da4a8f20d1752dd3
Vik Sharma <shar6770@mylaurier.ca>
2019-02-01 10:40:26 -0500

    results page will display uuid text input on top of the page

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (request, response) =>  response.sendFile(`${__dirname}/index.html`
 app.post('/', (request, response) => { //on form submit use post request
   console.log(request.body);
   console.log(request.body.name);
-  response.sendFile(`${__dirname}/results.html`);
+  response.render(__dirname + "/index.ejs", {result:request.body.name});
 });
 
 app.listen(5000, () => console.info('Application running on port 5000'));
